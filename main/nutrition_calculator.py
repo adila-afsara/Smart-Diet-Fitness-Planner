8e699d990c7be768e93f661d965e86faca47cabd
Adila Afsara <adilaafsara50@gmail.com>
2026-07-31 02:13:30 +0600

    Update nutrition_calculator.py
    BMR calculator

--- main/nutrition_calculator.py
+++ main/nutrition_calculator.py
@@ -1,1 +1,14 @@
+def calculate_bmr(weight, height, age, gender):
+    """
+    Calculate Basal Metabolic Rate (BMR)
+    using the Mifflin-St Jeor Equation.
+    """
 
+    gender = gender.lower()
+
+    if gender == "male":
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) + 5
+    else:
+        bmr = (10 * weight) + (6.25 * height) - (5 * age) - 161
+
+    return round(bmr)
