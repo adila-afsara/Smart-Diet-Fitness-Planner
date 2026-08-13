d9d3e01d79161927eee16b909ef0b6d4379cf8dd
Adiba Afsara <adibaafsara.work@gmail.com>
2026-08-13 10:04:41 +0600

    Update fitness_strategies.py

--- main/fitness_strategies.py
+++ main/fitness_strategies.py
@@ -1,1 +1,32 @@
+from abc import ABC, abstractmethod
 
+
+class FitnessStrategy(ABC):
+
+    @abstractmethod
+    def generate_plan(self, user_profile):
+        pass
+
+
+class BeginnerStrategy(FitnessStrategy):
+
+    def generate_plan(self, user_profile):
+        from .agents import fitness_agent
+
+        return fitness_agent(user_profile)
+
+
+class IntermediateStrategy(FitnessStrategy):
+
+    def generate_plan(self, user_profile):
+        from .agents import fitness_agent
+
+        return fitness_agent(user_profile)
+
+
+class AdvancedStrategy(FitnessStrategy):
+
+    def generate_plan(self, user_profile):
+        from .agents import fitness_agent
+
+        return fitness_agent(user_profile)
