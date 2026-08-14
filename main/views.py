01448c80cc676d9212ddf177cbb0d8cdaf659003
Adiba Afsara <adibaafsara.work@gmail.com>
2026-08-14 08:29:16 +0600

    Update views.py

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from .models import DailyLog, BMIRecord, WeeklyReport, DietPlan
 import traceback
 from django.db.models import Q
 from django.contrib import messages
