2035b2b2e1187f8af25d4e7239fc7eaec3026117
Adila Afsara <adilaafsara50@gmail.com>
2026-08-22 07:01:39 +0600

    Update settings.py
    SMTP gmail ( Reset Password )

--- core/settings.py
+++ core/settings.py
@@ -121,3 +121,16 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# ==============================
+# Email Configuration (SMTP)
+# ==============================
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'your_email@gmail.com'
+EMAIL_HOST_PASSWORD = 'your app pass word'
